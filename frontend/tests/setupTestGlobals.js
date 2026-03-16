7d1c0ad42cc123b4daa1b1b04fd94b20fca3eec9
Nikhil <nikhilsgond@gmail.com>
2026-03-16 14:37:37 +0530

    Delete Test Files

--- frontend/tests/setupTestGlobals.js
+++
@@ -1,5 +0,0 @@
-// tests/setupTestGlobals.js
-import { isConnectable } from '../src/shared/lib/shapeUtils.js';
-
-// Expose for legacy tests that call isConnectable globally
-global.isConnectable = isConnectable;
